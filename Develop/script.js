060ea6a3b2f369f90d924bb4794f8e6f4e557c2a
Adam Arthur <adamarthur44@gmail.com>
2021-03-21 18:25:38 +0000

    declared function called generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const generatePassword = function() {};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
