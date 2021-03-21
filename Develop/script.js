7dda994149b6339ac0e7bb011c6a97b81a471fb9
Adam Arthur <adamarthur44@gmail.com>
2021-03-21 19:56:26 +0000

    added prompts to decide password

--- Develop/script.js
+++ Develop/script.js
@@ -97,6 +97,9 @@ let isUpperCase;
 let isNumericChar;
 let isSpecialChar;
 
+// Declare Newly Created Array
+const chosenCharArray = [];
+
 //Generate Password 
 function generatePassword() {
   let passwordLength = prompt("Whats is the length of your password?");
@@ -107,16 +110,54 @@ function generatePassword() {
 
   if (passwordLength > 8 && passwordLength < 128) {
     console.log("Hurray");
+    // Confirm whether Password contains lowercase Characters
+    isLowerCase = confirm(
+      "Do you want your password to contain lowercase characters?"
+    );
+    console.log(isLowerCase);
+    // Confirm whether Password contains Uppercase Characters
+    isUpperCase = confirm(
+      "Do you want your password to contain uppercase characters?"
+    );
+    console.log(isUpperCase);
+    // Confirm whether Password contains Numeric Characters
+    isNumericChar = confirm(
+      "Do you want your password to contain Numeric characters?"
+    );
+    console.log(isNumericChar);
+    // Confirm whether Password contains Special Characters
+    isSpecialChar = confirm(
+      "Do you want your password to contain Special characters?"
+    )
+    console.log(isSpecialChar);
   }
   else
     alert("Password needs to be between 8 and 128!");
 
+  if (isLowerCase) {
+      chosenCharArray.push(lowerCaseArray);
+    } 
+  if (isUpperCase) {
+      chosenCharArray.push(upperCaseArray);
+    } 
+  if (isNumericChar) {
+      chosenCharArray.push(numericArray);
+    } 
+  if (isSpecialChar) {
+      chosenCharArray.push(specialCharArray);
+    } 
+  if (!lowerCaseArray && !isUpperCase && !isNumericChar && !isSpecialChar) {
+      alert("At least one type of Character needs to be selected")
+    } 
+    console.log(chosenCharArray)
+    
   // Create a variable to store Password
   var password = "hello";
 
   // Return Password
   return password;
 }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -126,6 +167,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button  
 generateBtn.addEventListener("click", writePassword);
